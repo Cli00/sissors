e87a3b6b03e6a1166a28ae4dbef63d2569be32e2
cli00 <ciintonidegu@gmail.com>
2024-03-11 18:09:55 +0100

    update

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgres://sicssor_user:nO3ERfPBizh165nj36uUJ4CacqKbWV0c@dpg-cnni20ed3nmc73cujed0-a/sicssor"
+SQLALCHEMY_DATABASE_URL = "postgres://sicssor_user:nO3ERfPBizh165nj36uUJ4CacqKbWV0c@dpg-cnni20ed3nmc73cujed0-a.oregon-postgres.render.com/sicssor"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
