addf6d17b8190a4ca43569c3ec0d73d3fdbf9dff
cli00 <ciintonidegu@gmail.com>
2024-03-15 12:39:25 +0100

    UPDATE

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:blank@localhost:5432/Scissor"
+SQLALCHEMY_DATABASE_URL = "postgresql://scissor_test_user:pvat8pRnTK7pflWQiqb4MNyTc1fPSZbd@dpg-cno200fsc6pc73bavvc0-a/scissor_test"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
